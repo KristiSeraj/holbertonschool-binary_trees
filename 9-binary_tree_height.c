ee28ddb40c4f9df003c3c010ec873e3550767bb7
KristiSeraj <serajkristi@gmail.com>
2022-07-19 04:34:34 -0700

    depth and height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,26 +1,29 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
- * Return: the number of height or 0 if tree is NULL
+ * max_height - find the maximum value between two variables
+ * @a: first num
+ * @b: second num
+ * Return: the max value
  */
 size_t max_height(size_t a, size_t b)
 {
-	if (a > b)
+	if (a >= b)
 		return (a);
 	else
 		return (b);
 }
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: the number of height or 0 if tree is NULL
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t r, l;
 
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
-	else
-	{
-		l = binary_tree_height(tree->left);
-		r = binary_tree_height(tree->right);
-		return (max_height(l, r) + 1);
-	}
+	l = binary_tree_height(tree->left);
+	r = binary_tree_height(tree->right);
+	return (max_height(l, r) + 1);
 }
