a1fd493d6d7698a26e5f090187eefabd370db44c
KristiSeraj <serajkristi@gmail.com>
2022-07-20 01:29:20 -0700

    fix

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,7 +6,7 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree->parent == NULL)
+	if (tree->parent == NULL || tree == NULL)
 		return (0);
 	return (1 + binary_tree_depth(tree->parent));
 }
