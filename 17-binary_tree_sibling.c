0b1f55a918b1417767f79894a9d1ee2cdaa3efb3
KristiSeraj <serajkristi@gmail.com>
2022-07-21 02:01:47 -0700

    fix

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,11 +10,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	if (node->parent->left != node)
-		if (node->parent->left != NULL)
-			return (node->parent->left);
-	if (node->parent->right != node)
-		if (node->parent->right != NULL)
-			return (node->parent->right);
+	if (node->parent->left != node && node->parent->left != NULL)
+		return (node->parent->left);
+	if (node->parent->right != node && node->parent->right != NULL)
+		return (node->parent->right);
 	return (NULL);
 }
